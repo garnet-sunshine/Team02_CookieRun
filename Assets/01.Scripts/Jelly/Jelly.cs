d5d0c90031fbe5b7c02456cac3e11d6eee7b53b8
Duek1 <ppsik22@gmail.com>
2025-05-12 11:02:21 +0900

    -add : 클래스수정

--- Assets/01.Scripts/Jelly/Jelly.cs
+++ Assets/01.Scripts/Jelly/Jelly.cs
@@ -5,28 +5,8 @@ using UnityEngine;
 public class Jelly : MonoBehaviour
 {
     public GameObject[] coinsPrefab; // 코인 프리펩 연결
-    public int coinCount = 10; // 코인을 몇개 생성할지
-    public float spacing = 1.5f; // 코인 간격
     public int scoreValue = 10; // 젤리 1개당 점수
 
-    private void Start()
-    {
-        SpawnCoins();
-    }
-
-    void SpawnCoins()
-    {
-        Vector3 startPos = transform.position; // 시작 위치
-
-        for (int i = 0; i < coinCount; i++)
-        {
-            int randomIndex = Random.Range(0, coinsPrefab.Length);
-            GameObject prefabToSpawn = coinsPrefab[randomIndex];
-
-            Vector3 spawnPos = new Vector3(startPos.x + (i * spacing), startPos.y, startPos.z);
-            Instantiate(prefabToSpawn, spawnPos, Quaternion.identity);
-        }
-    }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
